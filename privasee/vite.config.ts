a8940391acce6dc62df743a69a16b2bbe80f47a6
mvusic23 <mvusic23@studnet.foi.hr>
2026-04-28 23:47:06 +0200

    github pages

--- privasee/vite.config.ts
+++ privasee/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-})
+  base: mode === 'production' ? process.env.VITE_BASE_PATH || '/' : '/',
+}))
